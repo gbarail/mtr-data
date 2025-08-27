c164f8194d7ff09187e65543481ac10d9991788a
mtrhk <76126473+mtrhk@users.noreply.github.com>
2025-08-27 23:26:15 +0800

    feat: count MTR stations; modified output format

--- open-data/src/inspect_mtr_lines_fares.py
+++ open-data/src/inspect_mtr_lines_fares.py
@@ -7,6 +7,10 @@ data_file = CURDIR.parent / "data" / \
     "MTR routes, fares and barrier-free facilities" / "mtr_lines_fares.csv"
 
 
+def mtr_stations(df: pd.DataFrame):
+    return df['SRC_STATION_NAME'].nunique()
+
+
 def mtr_fare_stats(df: pd.DataFrame, key: str):
     data = df[df[key] != 0][key]  # Remove costs that are zero
     return {
@@ -18,13 +22,21 @@ def mtr_fare_stats(df: pd.DataFrame, key: str):
 
 def main():
     df = pd.read_csv(data_file)
+
+    # Count MTR Stations
+    print("Number of MTR stations:", mtr_stations(df))
+
+    # Fare Statistics
     item_keys = {
         "Adult Octopus Fare": "OCT_ADT_FARE",
         "Adult Single Journey Ticket Fare": "SINGLE_ADT_FARE",
     }
-    for item, key in item_keys.items():
-        stats = mtr_fare_stats(df, key)
-        print(f"[{item}]\n{json.dumps(stats, ensure_ascii=False)}")
+    fare_statistics = {
+        item: mtr_fare_stats(df, key)
+        for item, key in item_keys.items()
+    }
+    print("Fare Statistics:",
+          json.dumps(fare_statistics, ensure_ascii=False, indent=2))
 
 
 if __name__ == "__main__":
